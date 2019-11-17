b21cfed5424583500cb6ea1a5d72ce684e895cd5
michalkoziara <mkoziara12@gmail.com>
2019-11-17 17:49:59 +0100

    Add routing to dict

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const forceSSL = function() {
   }
 };
 
+app.use(express.static(__dirname + '/dist/IoT-front/'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/IoT-front/index.html'));
 });
 
 app.use(forceSSL());
-app.use(express.static(__dirname + '/dist/IoT-front/'));
 
 app.listen(process.env.PORT || 8080);
