ea91df03f99306104349699678bef99914b6beba
michalkoziara <mkoziara12@gmail.com>
2019-11-21 21:37:00 +0100

    Remove base href

--- postinstall.sh
+++ postinstall.sh
@@ -2,13 +2,13 @@
 
 echo "Building Angular app for $NODE_ENV"
 
-build_dev='ng build --aot --prod --base-href /app/dist/IoT-front/ --configuration development'
+build_dev='ng build --aot --prod --configuration development'
 if [ "$CONFIGURATION" = "development" ]; then
  echo "running $build_dev ..."
  eval "$build_dev"
 fi
 
-build_prod='ng build --aot --prod --base-href /app/dist/IoT-front/ --configuration production'
+build_prod='ng build --aot --prod --configuration production'
 if [ "$CONFIGURATION" = "production" ]; then
  echo "running $build_prod ..."
  eval "$build_prod"
