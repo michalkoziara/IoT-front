407e52cea5e53528ddc170ff1f3849ba92f6ce8c
PKramek <37769346+PKramek@users.noreply.github.com>
2019-11-23 00:03:49 +0100

    Add user registration

---
+++ src/app/services/userRegistration/user-registration.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {UserRegistrationRequest} from './user-registration-request';
+import {environment} from '../../../environments/environment';
+import {map} from 'rxjs/operators';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+const httpOptions = {
+  observe: 'response' as 'body',
+  headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserRegistrationService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  register(userData: UserRegistrationRequest) {
+    return this.http.post<any>(`${environment.apiUrl}/users`, userData, httpOptions).pipe(map(response => {
+        return response;
+      },
+      (data: any) => {
+        console.log(data);
+      }
+    ))
+      ;
+  }
+
+}
