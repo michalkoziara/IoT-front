3f5ff1c3ff187658930e60ec813b4a7c31574daa
PKramek <37769346+PKramek@users.noreply.github.com>
2019-11-24 19:13:12 +0100

    add components to admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,18 +2,31 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {AdminRoutingModule, routingComponents} from './admin-routing.module';
-import { AdminComponent } from './admin.component';
-import { SideNavComponent } from './components/side-nav/side-nav.component';
+import {AdminComponent} from './admin.component';
+import {SideNavComponent} from './components/side-nav/side-nav.component';
 import {AngularMaterialModule} from '../angular-material.module';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {AdminStartCardComponent} from './components/admin-start-card/admin-start-card.component';
+import {SharedModule} from "../shared/shared.module";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {JwtInterceptor} from "../services/authService/auth.interceptor";
+import {ErrorInterceptor} from "../services/authService/error.interceptor";
+import {AdminWelcomeService} from "./services/adminWelcomeService/admin-welcome.service";
+import { AdminInnerToolbarComponent } from './components/admin-inner-toolbar/admin-inner-toolbar.component';
 
 @NgModule({
-  declarations: [routingComponents, AdminComponent, SideNavComponent, DashboardComponent],
+  declarations: [routingComponents, AdminComponent, SideNavComponent, DashboardComponent, AdminStartCardComponent, AdminInnerToolbarComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
     AngularMaterialModule,
+    SharedModule,
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    AdminWelcomeService
+  ],
 })
-export class AdminModule { }
+export class AdminModule {
+}
