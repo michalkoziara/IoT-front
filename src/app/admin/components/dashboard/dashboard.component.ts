3a78edd06c746f69b025080ced2c709939dfcf5d
PKramek <37769346+PKramek@users.noreply.github.com>
2019-11-24 17:32:35 +0100

    Update dashboard component and admin welcome service

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {AdminWelcomeService} from '../../services/adminWelcomeService/admin-welcome.service';
+
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  isGetUsersGroupsListButtonClicked: boolean;
+  isGetUsersGroupsListButtonClickedSubscription: Subscription;
 
-  ngOnInit() {
+  isGetSensorsListButtonClicked: boolean;
+  isGetSensorsListButtonClickedSubscription: Subscription;
+
+  isGetDeviceListButtonClicked: boolean;
+  isGetDeviceListButtonClickedSubscription: Subscription;
+
+  isGetUnconfiguredListButtonClicked: boolean;
+  isGetUnconfiguredListButtonClickedSubscription: Subscription;
+
+  isGetSensorTypesListButtonClicked: boolean;
+  isGetSensorTypesListButtonClickedSubscription: Subscription;
+
+  isGetDevicesTypesListButtonClicked: boolean;
+  isGetDevicesTypesListButtonClickedSubscription: Subscription;
+
+  constructor(private welcomeService: AdminWelcomeService) {
   }
 
+  ngOnInit() {
+    this.isGetUsersGroupsListButtonClickedSubscription = this.welcomeService.isGetUsersGroupsListButtonClicked$.subscribe(
+      x => this.isGetUsersGroupsListButtonClicked = x
+    );
+    this.isGetSensorsListButtonClickedSubscription = this.welcomeService.isGetSensorsListButtonClicked$.subscribe(
+      x => this.isGetSensorsListButtonClicked = x
+    );
+    this.isGetDeviceListButtonClickedSubscription = this.welcomeService.isGetDeviceListButtonClicked$.subscribe(
+      x => this.isGetDeviceListButtonClicked = x
+    );
+    this.isGetUnconfiguredListButtonClickedSubscription = this.welcomeService.isGetUnconfiguredListButtonClicked$.subscribe(
+      x => this.isGetUnconfiguredListButtonClicked = x
+    );
+    this.isGetSensorTypesListButtonClickedSubscription = this.welcomeService.isGetSensorTypesListButtonClicked$.subscribe(
+      x => this.isGetSensorTypesListButtonClicked = x
+    );
+    this.isGetDevicesTypesListButtonClickedSubscription = this.welcomeService.isGetDevicesTypesListButtonClicked$.subscribe(
+      x => this.isGetDevicesTypesListButtonClicked = x
+    );
+
+  }
+
+  ngOnDestroy() {
+    this.isGetUsersGroupsListButtonClickedSubscription.unsubscribe();
+    this.isGetSensorsListButtonClickedSubscription.unsubscribe();
+    this.isGetDeviceListButtonClickedSubscription.unsubscribe();
+    this.isGetUnconfiguredListButtonClickedSubscription.unsubscribe();
+    this.isGetSensorTypesListButtonClickedSubscription.unsubscribe();
+    this.isGetDevicesTypesListButtonClickedSubscription.unsubscribe();
+  }
 }
--- src/app/admin/services/adminWelcomeService/admin-welcome.service.ts
+++ src/app/admin/services/adminWelcomeService/admin-welcome.service.ts
@@ -1,9 +1,7 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AdminWelcomeService {
   private isGetUsersGroupsListButtonClickedSource = new BehaviorSubject(false);
   private isGetSensorsListButtonClickedSource = new BehaviorSubject(false);
